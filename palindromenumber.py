34d3ad0f339ef0bbb7c996c1cef91939ea079730
Himanshu Saini <sainihimanshu.1999@gmail.com>
2021-02-21 18:38:54 +0530

    leetcode practice

---
+++ palindromenumber.py
@@ -0,0 +1,12 @@
+def isPalindrome(self, x):
+    """
+    :type x: int
+    :rtype: bool
+    """
+    if x<0:
+        return False
+    p, res = x ,0
+    while p:
+        res = res*10 + p%10
+        p /= 10
+    return res  == x

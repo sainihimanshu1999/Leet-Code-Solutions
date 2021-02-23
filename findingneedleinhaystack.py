b4f2d1b9a72c695c7ca53a3849e1e38defa589ba
Himanshu Saini <sainihimanshu.1999@gmail.com>
2021-02-23 20:31:35 +0530

    leetcode practice

---
+++ findingneedleinhaystack.py
@@ -0,0 +1,9 @@
+def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        if(needle not in haystack):
+            return(-1)
+        return(haystack.index(needle))
